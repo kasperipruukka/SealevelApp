422af6e3b0a96c2fec17e7885fe44594aae636e1
Kasperi Pruukka <55744887+kasperipruukka@users.noreply.github.com>
2023-09-20 23:21:51 +0300

    Ilmatieteenlaitos muutti apia
    Korjattu api kutsu.

--- src/shared/state/slices/sealevel/actions.ts
+++ src/shared/state/slices/sealevel/actions.ts
@@ -4,13 +4,8 @@ import { ApiData } from "src/shared/types/apiData";
 import wretch from 'wretch';
 
 export const getApiData = createAsyncThunk("getApiData",
-    async (): Promise<ApiData> => {
-        // Yesterdays time.
-        const startTime = Math.round((new Date().getTime() / 1000) - 3600);
-        // End of the week.
-        const endTime = Math.round((addDays(new Date(), 7).getTime() / 1000));
-        
-        const url = `https://www.ilmatieteenlaitos.fi/api/weather/sealevelgraphs?geoid=-10022816&fctstarttime=${startTime}&fctendtime=${endTime}&fcttimestep=60`;
+    async (): Promise<ApiData> => {        
+        const url = `https://www.ilmatieteenlaitos.fi/api/weather/sealevelgraphs/short?geoid=-10022816`;
         const res = await wretch(url).get().json() as ApiData;
         return res;
     }
